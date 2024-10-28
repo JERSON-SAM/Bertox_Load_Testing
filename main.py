7ed94d8386196009f310398cd0206eb2a8788fc3
JERSON-SAM <butawanjerson@gmail.com>
2024-10-28 16:14:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def bertoxcalculator():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Simple Calculator</title>
+        <title>TAN LOAD TESTING </title>
         <style>
             body { font-family: Arial, sans-serif; }
             form { margin: 20px; }
